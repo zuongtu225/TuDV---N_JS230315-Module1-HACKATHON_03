b2c43e87445657d1aae3a8815c1a2d59d3126be4
Dương Văn Tú <dvtu225@gmail.com>
2023-08-09 10:45:52 +0700

    Hackathon3

---
+++ noteApp.ts
@@ -0,0 +1,69 @@
+class Note {
+  getNote() {
+    let title = (document.getElementById("title") as HTMLInputElement).value;
+    let content = (document.getElementById("content") as HTMLInputElement)
+      .value;
+    if (listNoteLocal.length == 0) {
+      const newNote: object = { id: 1, title: title, content: content };
+      return newNote;
+    } else {
+      const maxId = Math.max(...listNoteLocal.map((note: any) => note.id));
+      const newNote: object = { id: maxId + 1, title: title, content: content };
+      return newNote;
+    }
+  }
+}
+const getnote = new Note();
+
+const listNote = localStorage.getItem("listNote");
+const listNoteLocal = listNote ? JSON.parse(listNote) : [];
+
+const add = document.getElementById("addNote") as HTMLElement;
+add.addEventListener("click", () => {
+  const newNote = getnote.getNote();
+  const checkForm = validate();
+  if (checkForm) {
+    alert("Vui lòng nhập đầy đủ thông tin");
+  } else {
+    listNoteLocal.push(newNote);
+    localStorage.setItem("listNote", JSON.stringify(listNoteLocal));
+    renderNote(listNoteLocal);
+  }
+});
+
+function renderNote(data: any) {
+  const print = document.getElementById("listNote") as HTMLDivElement;
+  let content = "";
+  data.forEach((note: any) => {
+    content += ` <div id="note">
+    <p id="renderTitle">${note.title}</p>    
+    <p id="noted">${note.content}</p>
+      <i class="bx bxs-trash-alt" id="delete" onclick="handleDelete(${note.id})" ></i>
+    </div>`;
+  });
+  print.innerHTML = content;
+}
+
+renderNote(listNoteLocal);
+
+function handleDelete(id: number) {
+  const idNote = listNoteLocal.find((note: any) => note.id === id);
+  console.log(idNote);
+  listNoteLocal.forEach((note: any, index: number) => {
+    if (idNote.id === note.id) {
+      listNoteLocal.splice(index, 1);
+    }
+  });
+  localStorage.setItem("listNote", JSON.stringify(listNoteLocal));
+  renderNote(listNoteLocal);
+}
+
+function validate() {
+  let title = (document.getElementById("title") as HTMLInputElement).value;
+  let content = (document.getElementById("content") as HTMLInputElement).value;
+  let isDulicate = false;
+  if (title === "" && content === "") {
+    isDulicate = true;
+  }
+  return isDulicate;
+}
---
+++ src/noteApp.js
@@ -0,0 +1,66 @@
+"use strict";
+class Note {
+    getNote() {
+        let title = document.getElementById("title").value;
+        let content = document.getElementById("content")
+            .value;
+        if (listNoteLocal.length == 0) {
+            const newNote = { id: 1, title: title, content: content };
+            return newNote;
+        }
+        else {
+            const maxId = Math.max(...listNoteLocal.map((note) => note.id));
+            const newNote = { id: maxId + 1, title: title, content: content };
+            return newNote;
+        }
+    }
+}
+const getnote = new Note();
+const listNote = localStorage.getItem("listNote");
+const listNoteLocal = listNote ? JSON.parse(listNote) : [];
+const add = document.getElementById("addNote");
+add.addEventListener("click", () => {
+    const newNote = getnote.getNote();
+    const checkForm = validate();
+    if (checkForm) {
+        alert("Vui lòng nhập đầy đủ thông tin");
+    }
+    else {
+        listNoteLocal.push(newNote);
+        localStorage.setItem("listNote", JSON.stringify(listNoteLocal));
+        renderNote(listNoteLocal);
+    }
+});
+function renderNote(data) {
+    const print = document.getElementById("listNote");
+    let content = "";
+    data.forEach((note) => {
+        content += ` <div id="note">
+    <p id="renderTitle">${note.title}</p>    
+    <p id="noted">${note.content}</p>
+      <i class="bx bxs-trash-alt" id="delete" onclick="handleDelete(${note.id})" ></i>
+    </div>`;
+    });
+    print.innerHTML = content;
+}
+renderNote(listNoteLocal);
+function handleDelete(id) {
+    const idNote = listNoteLocal.find((note) => note.id === id);
+    console.log(idNote);
+    listNoteLocal.forEach((note, index) => {
+        if (idNote.id === note.id) {
+            listNoteLocal.splice(index, 1);
+        }
+    });
+    localStorage.setItem("listNote", JSON.stringify(listNoteLocal));
+    renderNote(listNoteLocal);
+}
+function validate() {
+    let title = document.getElementById("title").value;
+    let content = document.getElementById("content").value;
+    let isDulicate = false;
+    if (title === "" && content === "") {
+        isDulicate = true;
+    }
+    return isDulicate;
+}
